3830fdfcd62ca1206dd3f39a7911c0c791d86f29
Guilherme Moura <guime.sprint@gmail.com>
2015-11-30 18:26:10 -0300

    Added a Framework target with shared scheme.

---
+++ DGRunkeeperSwitch/DGRunkeeperSwitch.h
@@ -0,0 +1,19 @@
+//
+//  DGRunkeeperSwitch.h
+//  DGRunkeeperSwitch
+//
+//  Created by Guilherme Moura on 11/30/15.
+//  Copyright © 2015 Danil Gontovnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DGRunkeeperSwitch.
+FOUNDATION_EXPORT double DGRunkeeperSwitchVersionNumber;
+
+//! Project version string for DGRunkeeperSwitch.
+FOUNDATION_EXPORT const unsigned char DGRunkeeperSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGRunkeeperSwitch/PublicHeader.h>
+
+
